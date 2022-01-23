03ba3ae6e61c5ceb8e98aec8eb6107128402fbf8
Terell Luna <terell.luna@yahoo.com>
2022-01-23 00:09:14 -0600

    added user input for tax year, should cache price data once it is fetched for a year so it can be served to users more quickly

--- pricefetcher.py
+++ pricefetcher.py
@@ -1,6 +1,23 @@
 import requests
+import datetime
 
-api = "https://api.cryptowat.ch/markets/kraken/ethusd/ohlc?after=1609459200&before=1640995199&periods=86400"
-response = requests.get(api)
+# get user tax year
+year = input("Enter the year for which you wish to calculate your tax liability: ")
 
+# after time is the beginning of the year
+aftertime = datetime.datetime(int(year), 1, 1, 0, 0, 0).timestamp()
+
+# before time is the end of the year
+beforetime = datetime.datetime(int(year), 12, 31, 11, 59, 59).timestamp()
+
+# set the payload using the aftertime, beforetime, and the period (day)
+payload = {"before": int(beforetime), "after": int(aftertime), "periods": 86400}
+
+# api url
+priceAPI = "https://api.cryptowat.ch/markets/kraken/ethusd/ohlc"
+
+# request call and response capture
+response = requests.get(priceAPI, params=payload)
+
+# display price data
 response.json()
