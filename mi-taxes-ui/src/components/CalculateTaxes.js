15abe09d9d6d5bcebfdea7481a73c9b15e95aaeb
Terell Luna <terell.luna@yahoo.com>
2022-05-07 02:48:36 -0500

    fixed taxcalc request

--- mi-taxes-ui/src/components/CalculateTaxes.js
+++ mi-taxes-ui/src/components/CalculateTaxes.js
@@ -1,7 +1,20 @@
 const CalculateTaxes = () => {
+    function calcTaxes(){
+        fetch('http://localhost:5000/getAccountIncomeByYearEth/0xc40064b67833b8a3ae28eb2ba3fcaadd88f17e4b/2021')
+        .then(response => {
+            if(!response.ok){
+                throw new Error(`HTTP error: ${response.status}`)
+            }
+            return response.json()
+        })
+        .then(response => {
+            console.log(response)
+        })
+    }
+
     return (
         <>
-            Tax Calc stuff here
+            <button onClick={calcTaxes}>Skippa Da Clicka</button>>
         </>
     )
 }
